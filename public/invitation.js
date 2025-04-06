4ca73d60b1c47d9e18415848a0de0aef18cef99a
Valerio-liuz <liuz2602@gmail.com>
2025-04-06 17:08:07 +0700

    memperbaiki count down

--- public/invitation.js
+++ public/invitation.js
@@ -33,21 +33,22 @@ let x = setInterval(function () {
     let now = new Date().getTime();
 
     let distance = countDownDate - now;
-
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById('day').innerHTML = days + ' d'
-    document.getElementById('hour').innerHTML = hours + ' h'
-    document.getElementById('minute').innerHTML = minutes + ' m'
-    document.getElementById('second').innerHTML = seconds + ' s'
-
+    
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        document.getElementById("count-down").innerHTML = "Event done, thank your for participation"
     }
-}, 1000);
+    else{
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        document.getElementById('day').innerHTML = days + ' d'
+        document.getElementById('hour').innerHTML = hours + ' h'
+        document.getElementById('minute').innerHTML = minutes + ' m'
+        document.getElementById('second').innerHTML = seconds + ' s'    
+    }
+    }, 1000);
 
 $(document).ready(function () {
     const $contentDiv = $("#msg-list");
